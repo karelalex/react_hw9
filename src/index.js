de976833c9d4521ec2b21a66adc09a1a2dc2efb6
Karelin Alexander <a.karelin@i-teco.ru>
2021-08-07 13:17:47 +0300

    Добавлено 3-ое упражнение

--- src/index.js
+++ src/index.js
@@ -1,36 +1,80 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const mega = {
-    ping: () => alert("Привет")
+// Обычные и колбэк рефы
+class SimpleRef extends React.PureComponent{
+    inputRef = React.createRef()
+    pElement = null
+    setPElement = el => this.pElement = el
+    componentDidMount() {
+        this.inputRef.current.value = 'Всё заработало'
+        this.pElement.innerText = 'И тут работает'
+    }
+
+    render() {
+        return (
+            <div>
+                <input ref={this.inputRef} />
+                <p ref={this.setPElement} />
+            </div>
+        )
+    }
 }
 
-const MegaContext = React.createContext(null)
+// проброс колбэк рефа в дочерний компонент и обманка с другим именем пропса
+class ChildRef extends React.PureComponent{
+    inputElement = null
+    setInputElement = el => this.inputElement = el
+    secondInputRef = React.createRef()
+    componentDidMount() {
+        this.inputElement.value = 'Я смог вставить текст в дочернем элементе'
+        this.secondInputRef.current.value = 'И тут всех обманул'
+    }
+    render() {
+        return(
+            <div>
+                <p>Вставляем потомку</p>
+                <ChildInput refProp={this.setInputElement} /><br />
+                <ChildInput refProp={this.secondInputRef} />
+            </div>
+        )
+    }
+}
 
-const Provider = ({value, children}) => (
-    <MegaContext.Provider value={value}>
-        {children}
-    </MegaContext.Provider>
-)
-const Sega = ({mega}) => (
-    <button onClick={mega.ping}>Жмакни меня</button>
-)
+const ChildInput = ({refProp}) => <input ref={refProp} />
 
-const Giper = () => (
+const ForwardedInput = React.forwardRef((props, ref) => (
     <div>
-        <h1>Я гипер</h1>
-        <MegaContext.Consumer>
-            {(mega) => (<Sega mega={mega}/>)}
-        </MegaContext.Consumer>
-
+        <p>Лучший инпут в городе</p>
+        <input ref={ref}/>
     </div>
-)
+
+))
+
+// Использование форвард реф.
+const ForwardRef = () => {
+    const ref = useRef()
+    const clickHandler = () => {
+        ref.current.value = 'меня кликнули'
+        ref.current.focus()
+    }
+    return (
+        <div>
+            <ForwardedInput ref={ref} />
+            <button onClick={clickHandler}>Жми</button>
+        </div>
+    )
+}
+
+
 
 function App() {
     return (
         <div className="App">
-            <Giper />
+            <SimpleRef />
+            <ChildRef />
+            <ForwardRef />
         </div>
     );
 }
@@ -38,9 +82,7 @@ function App() {
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider value={mega}>
             <App/>
-        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
