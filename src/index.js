349f39561837b743e8e5e38a9ef41e97f3a2f966
Karelin Alexander <a.karelin@i-teco.ru>
2021-08-14 14:08:42 +0300

    Добавлено 8-ое упражнение

--- src/index.js
+++ src/index.js
@@ -1,127 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Button, makeStyles, TextField} from "@material-ui/core";
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+import * as yup from 'yup'
 
-class Form extends React.PureComponent{
-    state = {
-        simpleInput: 'инпут',
-        textArea: 'Ария',
-        selector: 3,
-        multiSelector: [2,4],
-        checker: false,
-        radio: 'два'
-    }
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+}));
 
-    ref = React.createRef()
+const schema = yup.object().shape({
+    userName: yup.string()
+        .required('Поле обязательно для заполнения'),
+    password: yup.string()
+        .required('Поле обязательно для заполнения')
+        .min(5, 'Должно быть минимум 5 символов'),
+    email: yup.string()
+        .required('Поле обязательно для заполнения')
+        .email('Поле должно быть корректным адресом электронной почты')
 
-    changeHandler = (e) => {
-        const {name, value, type, selectedOptions, checked} = e.target
-        if (type === 'select-multiple') {
-            const newValues = Array.from(selectedOptions,option => option.value)
-            this.setState({
-                [name]: newValues
-            })
-            return
-        }
+})
 
-        if (type === 'checkbox') {
-           this.setState({
-               [name]: checked
-           })
-            return;
-        }
+const Form = () => {
+    const classes = useStyles();
+    const {register, handleSubmit, formState: {errors}} = useForm({
+        resolver: yupResolver(schema)
 
-        this.setState({
-            [name]: value
-        })
-    }
-
-    submitHandler = (e) => {
-        e.preventDefault()
-        console.log(this.ref.current.value)
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.submitHandler}>
-                <div>
-                    <h5>
-                        простой инпут
-                    </h5>
-                    <p>
-                        <input name="simpleInput" value={this.state.simpleInput} onChange={this.changeHandler} />
-                    </p>
-                </div>
-                <div>
-                    <h5>
-                        Текстовая ария
-                    </h5>
-                    <p>
-                        <textarea name="textArea" value={this.state.textArea} onChange={this.changeHandler} />
-                    </p>
-                </div>
-                <div>
-                    <h5>
-                        Селект без мультивыбора
-                    </h5>
-                    <p>
-                        <select name="selector" value={this.state.selector} onChange={this.changeHandler} >
-                            <option value={1}>Один</option>
-                            <option value={2}>Два</option>
-                            <option value={3}>Три</option>
-                            <option value={4}>Четыре</option>
-                            <option value={5}>Пять</option>
-                        </select>
-                    </p>
-                </div>
-                <div>
-                    <h5>
-                        Селект c мультивыбором
-                    </h5>
-                    <p>
-                        <select multiple name="multiSelector" value={this.state.multiSelector} onChange={this.changeHandler} >
-                            <option value={1}>Один</option>
-                            <option value={2}>Два</option>
-                            <option value={3}>Три</option>
-                            <option value={4}>Четыре</option>
-                            <option value={5}>Пять</option>
-                        </select>
-                    </p>
-                </div>
-                <h5>
-                    Чекбокс
-                </h5>
-                <p>
-                    <input type="checkbox" name="checker" checked={this.state.checker} onChange={this.changeHandler} />
-                </p>
-                <h5>
-                    Радиокнопка
-                </h5>
-                <p>
-                    Один <input type="radio" name="radio" checked={this.state.radio === 'один'} value="один" onChange={this.changeHandler} /> <br />
-                    Два <input type="radio" name="radio" checked={this.state.radio === 'два'} value="два" onChange={this.changeHandler} /> <br />
-                    Три <input type="radio" name="radio" checked={this.state.radio === 'три'} value="три" onChange={this.changeHandler} /> <br />
-                </p>
-                <div>
-                    <h5>
-                        Неконтролируемый инпут
-                    </h5>
-                    <p>
-                        <input name="uncontrolledInput" ref={this.ref} />
-                    </p>
-                    <p>
-                        <button type="submit">Послать</button>
-                    </p>
-                </div>
-            </form>
-        )
-    }
+    })
+    const onSubmit = data => console.log(data)
+    return (
+        <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+                id="name"
+                label="Имя" {...register('userName')}
+                helperText={errors.userName?.message}
+                error={errors.userName}
+            />
+            <TextField
+                id="email"
+                label="Электронная почта" {...register('email')}
+                helperText={errors.email?.message}
+                error={errors.email}
+            />
+            <TextField
+                id="pass"
+                label="Пароль"
+                type="password" {...register('password')}
+                helperText={errors.password?.message} error={errors.password}
+            />
+            <Button variant="contained" color="primary" type="submit">
+                Отправить
+            </Button>
+        </form>
+    );
 }
+
 
 function App() {
     return (
         <div className="App">
-            <Form />
+            <Form/>
         </div>
     );
 }
